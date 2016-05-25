1906830875bf21825e0fe1e6bab863ed71890a4e
Wei <Weiguxp@gmail.com>
2016-05-25 18:59:42 +0800

    sparkposting

--- ping2.py
+++ ping2.py
@@ -1,7 +1,7 @@
 import urllib2
 from HTMLParser import HTMLParser
 import smtplib
-# from sparkpost import SparkPost
+from sparkpost import SparkPost
 import time
 from datetime import datetime
 import requests
@@ -9,29 +9,39 @@ import base64
 
 #Settings for Email / SMS posting
 # sp = SparkPost('a0c04366cd1a5e02ac31f62e6ff4e9aa105afc2c')
+sentEmails = 0
+sentSMS = 0
 SMSpassword = base64.b64encode('123QWEasd')
 
 def sendSMS(targetnumber,errorMSG):
 	#Sends an SMS to target, notifying servers are down
-	try:
-		SMSHTTPPOST = 'http://61.147.98.117:9001/servlet/UserServiceAPI?method=sendSMS&isLongSms=0&username=18802671838&password=%s &smstype=1&extenno=123&mobile=%s&content=%s' % (SMSpassword, targetnumber, errorMSG)
-		req = requests.get(SMSHTTPPOST)
-		print req.text 
-	except:
-		print 'Error, cannot send SMS'
+	if sentSMS < 3:
+		try:
+			SMSHTTPPOST = 'http://61.147.98.117:9001/servlet/UserServiceAPI?method=sendSMS&isLongSms=0&username=18802671838&password=%s &smstype=1&extenno=123&mobile=%s&content=%s' % (SMSpassword, targetnumber, errorMSG)
+			req = requests.get(SMSHTTPPOST)
+			print req.text 
+			sentSMS += 1
+		except:
+			print 'Error, cannot send SMS'
+	else:
+		return 0
 
-# def sendErrorMail(mailMsg):
-# 	#Send an error email using SparkPost
-# 	try:
-# 		response = sp.transmissions.send(
-# 		    recipients=['weiguxp@gmail.com'],
-# 		    html='<p>Automated Message</p>',
-# 		    from_email='wei@weigu.org',
-# 		    subject= mailMsg
-# 		)
-# 		print(response)
-# 	except:
-# 		print 'error cannot send email'
+def sendErrorMail(targetmail, mailMsg):
+	#Send an error email using SparkPost
+	if sentEmails < 3:
+		try:
+			response = sp.transmissions.send(
+			    recipients=[targetmail],
+			    html='<p>Automated Message</p>',
+			    from_email='wei@weigu.org',
+			    subject= mailMsg
+			)
+			print(response)
+			sentEmails += 1 
+		except:
+			print 'error cannot send email'
+	else:
+		return 0
 
 
 def getReport(targetURL, numRetry = 0):
@@ -64,9 +74,9 @@ def monitorServers():
 	totalUpStats += getReport('http://2686.sanguochong.com.cn/cluster/status')
 	print 'got some Uptime reports, total is %i' % totalUpStats
 	if totalUpStats != 12:
-		# sendErrorMail('Some clusters are down')
-		sendSMS(13084686372, 'some servers are down')
-		sendSMS(15014090802, 'some servers are down')
+		sendErrorMail('weiguxp@gmail.com', 'Some clusters are down')
+		# sendSMS(13084686372, 'some servers are down')
+		# sendSMS(15014090802, 'some servers are down')
 
 
 
