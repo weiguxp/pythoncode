1005dc6556a25846e3430df2fe2d9c038f4c6e16
Wei <weiguxp@gmail.com>
2016-12-23 12:14:58 +0800

    SpawnGenerator before inherit

---
+++ SpawnGenerator/SpawnGen.py
@@ -0,0 +1,45 @@
+import random
+
+spawnList = []
+
+class SpawnRecord(object):
+	def __init__(self, waveId, spawnDelay, spawnPosition, monsterAsset, speed, hitPoint, atk, aiBehaviour):
+		self.waveId = int(waveId)
+		self.spawnDelay = int(spawnDelay)
+		self.spawnPosition = str(spawnPosition)
+		self.monsterAsset  = str(monsterAsset)
+		self.speed  = float(speed)
+		self.hitPoint  = int(hitPoint)
+		self.atk  = int(atk)
+		self.aiBehaviour = str(aiBehaviour)
+
+	def __str__(self):
+		return str(self.waveId) + "," + str(self.spawnDelay) + "," + self.spawnPosition + "," + self.monsterAsset + "," + str(self.speed) + "," + str(self.hitPoint) + "," + str(self.atk) + "," + self.aiBehaviour
+
+class gameMonster(object):
+	"""docstring for gameMonster"""
+	def __init__(self, monsterAsset, speed, hitPoint, atk, aiBehaviour):
+		self.arg = arg
+		
+
+def createSpawn():
+	mySpawn = SpawnRecord(1,5,randomSpawnPoint(),"Slime1",0.05,2,10,"test")
+	return mySpawn
+
+def randomSpawnPoint():
+	spawnpoints = ["-6.7;-0.2;17.9;", "18.1;-0.1;-.60;", "-30.2;4.0;36.3;", "41.7;4.0;-26.7;"]
+	chosenspawn = random.choice(spawnpoints)
+	return chosenspawn
+
+def randomSpawnTime(duration):
+	time = random.randint(1,duration)
+	return time
+
+
+
+spawnList = []
+for i in range(10):
+	spawnList.append(createSpawn())
+
+for i in spawnList:
+	print i
